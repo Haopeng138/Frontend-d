97cbb232513bcb652ecd05babd967b69b5fc20ab
haopeng.lin <haopeng.lin@est.fib.upc.edu>
2022-12-07 23:28:14 +0100

    Good

---
+++ datos (1)/pelicula.js
@@ -0,0 +1,19 @@
+<script type="application/ld+json">
+    {
+      "@context":"http://schema.org",
+      "@type":"Movie",
+      "name": "Metropoli",
+      "inLanguage": "de",
+      "wikipedia":{
+         "@type" : "URL",
+         "url":"https://en.wikipedia.org/wiki/Metropolis_(1927_film)",
+      },
+      "imdb":{
+         "@type":" URL",
+         "url":"https://www.imdb.com/title/tt0017136/fullcredits/?ref_=tt_ov_st_sm",
+      },
+
+      
+   }
+
+</script>
